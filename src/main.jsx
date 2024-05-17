8a25afeb06fa689cd670645ef053443aa02d1cbc
Aditya <aditya.verma2021@glbajajgroup.org>
2024-05-17 16:46:21 +0530

    react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from './App.jsx';
 import './index.css'
 // library import
 import {BrowserRouter} from 'react-rounter-dom'
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster/>
     </BrowserRouter>
 );
