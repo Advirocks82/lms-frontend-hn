43c6960e588017774edf633e12385f48d8dfbf6a
Aditya <aditya.verma2021@glbajajgroup.org>
2024-05-25 02:20:57 +0530

    Added Course lectures

--- src/Redux/Slices/CourseSlice.js
+++ src/Redux/Slices/CourseSlice.js
@@ -1,14 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-
 import { toast } from "react-hot-toast";
-
-
-
-
 import axiosInstance from "../../Helpers/axiosInstance";
-
-
-
 
 const initialState = {
 
@@ -16,34 +8,18 @@ const initialState = {
 
 }
 
-
-
-
 export const getAllCourses = createAsyncThunk("/course/get", async () => {
-
     try {
-
         const response = axiosInstance.get("/courses");
-
         toast.promise(response, {
-
             loading: "loading course data...",
-
             success: "Courses loaded successfully",
-
             error: "Failed to get the courses",
-
         });
 
-
-
-
         return (await response).data.courses;
-
     } catch(error) {
-
         toast.error(error?.response?.data?.message);
-
     }
 
 }); 
